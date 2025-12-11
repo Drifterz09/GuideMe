84e4ee254d09f79f6e7a855fec41d84ebe5df131
Drifterz09 <shauryagiri09@gmail.com>
2025-12-11 11:20:48 +0530

    Remove unnecessary blank lines in widget.py

--- widget.py
+++ widget.py
@@ -1,12 +1,1 @@
-import tkinter as tk
-from tkinter import messagebox
-root=tk.Tk()
 
-
-
-label=tk.Label(root,text="Welcome to GuideMe!",pady=30).pack()
-root.geometry("30x30")
-
-
-
-root.mainloop()
